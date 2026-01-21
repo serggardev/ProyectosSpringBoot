480138596a43957650f89540e950a15056114181
serggardev <sergio.gargoles@educa.madrid>
2026-01-21 13:07:54 +0100

    Actualizacion a java 21 y creacion de los dto

--- taskmanager/src/main/java/com/model/User.java
+++ taskmanager/src/main/java/com/model/User.java
@@ -29,7 +29,7 @@ public class User {
     @Id
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @Column(name="user_name", nullable=false, unique=true)
     private String userName;
